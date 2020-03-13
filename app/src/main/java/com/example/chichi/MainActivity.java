1f91a5ddaf3b77a9023ef510f4deaf69e3136f84
정상현 <jeongsanghyeon@jeongsanghyeon-ui-Macmini.local>
2020-03-13 18:02:29 +0900

    프레그먼트 하다 맒

--- app/src/main/java/com/example/chichi/MainActivity.java
+++ app/src/main/java/com/example/chichi/MainActivity.java
@@ -1,18 +1,32 @@
 package com.example.chichi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    Fragment fragment1;
+    Fragment fragment2;
+    Fragment fragment3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragment1 = new Fragment();
+        fragment2 = new Fragment();
+        fragment3 = new Fragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+        BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
     }
 
     @Override
