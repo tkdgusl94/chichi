409b64e2bc7691cba953356ed490ff8a7abcaf81
tkdgusl94 <tkdgusl94@gmail.com>
2020-03-15 22:15:03 +0900

    질문 포함

--- app/src/main/java/com/example/chichi/InfoFragment.java
+++ app/src/main/java/com/example/chichi/InfoFragment.java
@@ -16,3 +16,10 @@ public class InfoFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_info, container, false);
     }
 }
+
+
+/*
+질문
+1. 스타트 화면에서 권한을 받아야지만 다음 화면으로 가게끔 하려면?
+2. 메시지를 받으면 리시버에서 메시지 프래그먼트의 리사이클러뷰에 추가하는 방법
+ */
